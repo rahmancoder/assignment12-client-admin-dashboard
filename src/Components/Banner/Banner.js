72b1dc16021755f38d9d62f1bbe77cdf2010c13e
Mustafizur <sayhemrahman@gmail.com>
2021-11-12 00:49:08 +0600

    banner added backend and admin setting tommrorow

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+
+import banner1 from '../../images/Bannerimage/banner1.jpg';
+import banner2 from '../../images/Bannerimage/banner2.jpg';
+import banner3 from '../../images/Bannerimage/banner3.jpg';
+import banner4 from '../../images/Bannerimage/banner4.jpg';
+
+const Banner = () => {
+    return (
+        <div>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3> Drone Product</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner2}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Drone 3D for Harvesting</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner3}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Multidimention Drone </h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner4}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Drone Blog</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    );
+};
+
+export default Banner;
