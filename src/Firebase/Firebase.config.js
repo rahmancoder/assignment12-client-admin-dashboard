75c1dabe33fdc7e6729eb176e7853ea5a30125bc
Md Mustafizur Rahman Sayem <40750318+rahmancoder@users.noreply.github.com>
2021-11-27 20:22:25 +0600

    changed .env.local

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,19 +1,14 @@
 const firebaseConfig = {
 
-    // apiKey: process.env.REACT_APP_API_KEY,
-    // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    // projectId: process.env.REACT_APP_PROJECT_ID,
-    // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    // appId: process.env.REACT_APP_APP_ID,
+     apiKey: process.env.REACT_APP_API_KEY,
+     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+     projectId: process.env.REACT_APP_PROJECT_ID,
+     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+     appId: process.env.REACT_APP_APP_ID,
 
 
-    apiKey: "AIzaSyA9mkmwE08dRsFIRuZ1f7uh3inqKwZxL7I",
-    authDomain: "mustafiz-drone-website.firebaseapp.com",
-    projectId: "mustafiz-drone-website",
-    storageBucket: "mustafiz-drone-website.appspot.com",
-    messagingSenderId: "18974509658",
-    appId: "1:18974509658:web:b7b031645f570d15652663"
+
 };
 
 export default firebaseConfig;
